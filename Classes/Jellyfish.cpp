86b97436520fe6f9f33baf5c064f7c7e95e1677b
Cookrin <cookrin@liike.me>
2015-07-14 09:53:26 -0700

    create the Jellyfish class

---
+++ Classes/Jellyfish.cpp
@@ -0,0 +1,9 @@
+//
+//  Jellyfish.cpp
+//  jellyrun
+//
+//  Created by Tingting Gu on 2015/07/14.
+//
+//
+
+#include "Jellyfish.h"
---
+++ Classes/Jellyfish.h
@@ -0,0 +1,23 @@
+//
+//  Jellyfish.h
+//  jellyrun
+//
+//  Created by Tingting Gu on 2015/07/14.
+//
+//
+
+#ifndef __jellyrun__Jellyfish__
+#define __jellyrun__Jellyfish__
+
+#include "cocos2d.h"
+
+class Jellyfish : public cocos2d::Node
+{
+public:
+    CREATE_FUNC(Jellyfish);
+    
+protected:
+    
+};
+
+#endif /* defined(__jellyrun__Jellyfish__) */
