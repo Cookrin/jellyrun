a60a6a68e6446644b24b2ad5a8cf6fe8b1a54445
Cookrin <cookrin@liike.me>
2015-07-13 17:13:30 -0700

    add iPhone solutions

---
+++ Classes/Lobby.cpp
@@ -0,0 +1,9 @@
+//
+//  Lobby.cpp
+//  jellyrun
+//
+//  Created by Tingting Gu on 2015/07/13.
+//
+//
+
+#include "Lobby.h"
---
+++ Classes/Lobby.h
@@ -0,0 +1,14 @@
+//
+//  Lobby.h
+//  jellyrun
+//
+//  Created by Tingting Gu on 2015/07/13.
+//
+//
+
+#ifndef __jellyrun__Lobby__
+#define __jellyrun__Lobby__
+
+#include <stdio.h>
+
+#endif /* defined(__jellyrun__Lobby__) */
